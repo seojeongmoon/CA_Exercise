89b0b3c3396ce43b901259b12d7fe1388ca39659
Martijn <martijn.dendooven@outlook.com>
2023-04-20 10:26:20 +0200

    end of lab session1

--- CA_Exercises/Verilog/RTL/control_unit.v
+++ CA_Exercises/Verilog/RTL/control_unit.v
@@ -15,17 +15,20 @@ module control_unit(
    );
 
    // RISC-V opcode[6:0] (see RISC-V greensheet)
-   parameter integer ALU_R      = 7'b0110011;
-   parameter integer ALU_I      = 7'b0010011;
-   parameter integer BRANCH_EQ  = 7'b1100011;
-   parameter integer JUMP       = 7'b1101111;
-   parameter integer LOAD       = 7'b0000011;
-   parameter integer STORE      = 7'b0100011;
+   parameter integer ALU_R      = 7'b0110011; //33
+   parameter integer ALU_I      = 7'b0010011; //13
+   parameter integer BRANCH_EQ  = 7'b1100011; //63
+   parameter integer JUMP       = 7'b1101111; //6F
+   parameter integer LOAD       = 7'b0000011; //03
+   parameter integer STORE      = 7'b0100011; //23
 
    // RISC-V ALUOp[1:0] (see book Figure 4.12)
    parameter [1:0] ADD_OPCODE     = 2'b00;
    parameter [1:0] SUB_OPCODE     = 2'b01;
    parameter [1:0] R_TYPE_OPCODE  = 2'b10;
+   // parameter [1:0] BEQ_OPCODE     = 2'b01;
+   // parameter [1:0] LOAD_OPCODE    = 2'b00;
+   // parameter [1:0] STORE_OPCODE   = 2'b00;
 
    //The behavior of the control unit can be found in Chapter 4, Figure 4.18
 
@@ -42,8 +45,63 @@ module control_unit(
             alu_op    = R_TYPE_OPCODE;
             jump      = 1'b0;
          end
-         
+
          // Declare the control signals for each one of the instructions here...
+
+         ALU_I:begin
+            alu_src   = 1'b0;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b1;
+            mem_read  = 1'b0;
+            mem_write = 1'b0;
+            branch    = 1'b0;
+            alu_op    = ADD_OPCODE;
+            jump      = 1'b0;
+         end
+         
+         BRANCH_EQ:begin
+            alu_src   = 1'b0;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b0;
+            mem_read  = 1'b0;
+            mem_write = 1'b0;
+            branch    = 1'b1;
+            alu_op    = SUB_OPCODE;
+            jump      = 1'b0;
+         end
+
+         JUMP:begin
+            alu_src   = 1'b0;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b0;
+            mem_read  = 1'b0;
+            mem_write = 1'b0;
+            branch    = 1'b0;
+            alu_op    = R_TYPE_OPCODE;
+            jump      = 1'b1;
+         end
+
+         LOAD:begin
+            alu_src   = 1'b1;
+            mem_2_reg = 1'b1;
+            reg_write = 1'b1;
+            mem_read  = 1'b1;
+            mem_write = 1'b0;
+            branch    = 1'b0;
+            alu_op    = ADD_OPCODE;
+            jump      = 1'b0;
+         end
+
+         STORE:begin
+            alu_src   = 1'b1;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b0;
+            mem_read  = 1'b0;
+            mem_write = 1'b1;
+            branch    = 1'b0;
+            alu_op    = ADD_OPCODE;
+            jump      = 1'b0;
+         end
 
          default:begin
             alu_src   = 1'b0;
@@ -57,6 +115,8 @@ module control_unit(
          end
       endcase
    end
+
+   assign reg_dst = 1'b0;
 
 endmodule
 
